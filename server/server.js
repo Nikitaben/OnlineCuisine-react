e722b76c130d1ee855efeff0a98fcf70539ffa99
Nikitaben <nikitaprajapati88@gmail.com>
2021-10-10 15:57:32 -0600

    preparation for deployment

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const parser = require("body-parser");
 const cors = require("cors");
-const PORT = 3001;
+const PORT = process.env.PORT || 5000//3001;
 const app = express();
 app.use(parser.json());
 app.use(cors());
